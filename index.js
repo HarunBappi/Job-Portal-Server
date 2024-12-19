e60d2888e4f319e6d94b4b44630fc95cea0028df
Bappi1234 <harunbappi1234@gmail.com>
2024-12-19 18:24:18 +0600

    jwt added on server side

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // MiddleWare
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.40t0y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -35,6 +43,19 @@ async function run() {
     const jobApplicationCollection = client
       .db("jobPortal")
       .collection("job_application");
+
+    // Auth Related API's
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false, // secret : true only production base
+        })
+        .send({ success: true });
+    });
 
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
@@ -64,6 +85,7 @@ async function run() {
     app.get("/job_application", async (req, res) => {
       const email = req.query.email;
       const query = { application_email: email };
+      console.log("haha haha cookie", req.cookies);
       const result = await jobApplicationCollection.find(query).toArray();
 
       // Fhokira way to myApplications data Aggreged
